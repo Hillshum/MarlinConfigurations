6e4b29fd9994cb16432f0470e0b2f9b77dd50e62
Hilton Shumway <hillshum@gmail.com>
2024-09-06 21:51:47 -0500

    Adjust bed size and x max for sprite pro
    The x carrage can't travel as far down the gantry

--- config/examples/Creality/Ender-3 Pro/CrealityV427/Configuration.h
+++ config/examples/Creality/Ender-3 Pro/CrealityV427/Configuration.h
@@ -1577,14 +1577,14 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 235
+#define X_BED_SIZE 215
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 245
+#define X_MAX_POS 219
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
