5e94ce332898207c7ee66d6180b057d973a6826c
Hilton Shumway <hillshum@gmail.com>
2024-09-06 21:47:15 -0500

    Update probe offsets for sprite pro

--- config/examples/Creality/Ender-3 Pro/CrealityV427/Configuration.h
+++ config/examples/Creality/Ender-3 Pro/CrealityV427/Configuration.h
@@ -1378,7 +1378,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -45, -10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -29, -41, -3.48 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
